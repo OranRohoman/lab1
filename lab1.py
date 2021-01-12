b7dde147f9311017ef0a561531504d4d95dc292b
OranRohoman <54954506+OranRohoman@users.noreply.github.com>
2021-01-11 19:23:16 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,5 +3,6 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get('https://www.google.com')
+r = requests.get('https://raw.githubusercontent.com/OranRohoman/lab1/main/lab1.py')
 
+print(r.text)
